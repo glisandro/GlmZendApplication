a22ec117b1887d850d7f50f72c079c5330cf68be
Gustavo L. Martinez <gustavomartinez@gmail.com>
2011-11-20 13:16:14 +0100

    Primera versión funcional

---
+++ modules/Local/configs/module.config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'di' => array(
+        'instance' => array(
+            'doctrine_connection' => array(
+                'parameters' => array(
+                    'params' => array(
+                        'driver'   => 'pdo_mysql',
+                        'host'     => 'localhost',
+                        'port'     => '3306', 
+                        'user'     => 'root',
+                        'password' => '',
+                        'dbname'   => 'pruebas',
+                    ),
+                    'config' => 'doctrine_config',
+                    'evm'    => 'doctrine_evm'
+                )
+            ),
+        )
+    )
+);
